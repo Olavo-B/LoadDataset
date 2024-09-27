e755fb2e7b9a262b298f029eb12a86328a6b59b8
Olavo-B <olavo@ufv.br>
2024-09-27 10:26:40 -0300

    feat: Add load_kdd99 function to LoadDataset.py

--- LoadDataset/LoadDataset.py
+++ LoadDataset/LoadDataset.py
@@ -402,7 +402,7 @@ def load_iris(debug=False, save_path=None, load_path=None):
         df = pd.concat([y, X], axis=1)
 
         if debug:
-            print(f"Saving dataset to {save_path}")
+            print(f"Saving da   taset to {save_path}")
     
         # Save the dataset to the save path
         df.to_csv(os.path.join(save_path, 'iris.csv'), index=False)
@@ -416,3 +416,104 @@ def load_iris(debug=False, save_path=None, load_path=None):
         print("="*100)
     
     return X, y
+
+def load_kdd99(debug=False, save_path:str = None, load_path:str = None):
+
+    """
+    Load the KDD99 dataset from the UCI Machine Learning Repository.
+
+    :param debug: If True, only load a subset of the dataset for faster testing
+    :param save_path: If provided, save the dataset to this path
+    :param load_path: If provided, load the dataset from this path
+
+    :return: A tuple containing the data and target variables
+    
+    
+    """
+    
+    if load_path and os.path.exists(load_path):
+        print(f"Loading dataset from {load_path}")
+        df = pd.read_csv(os.path.join(load_path, 'kdd99.csv'))
+ 
+
+        # Rename the first column to 'target' and separate it as a Series
+        target = df.iloc[:, 0]
+        target.name = 'target'
+
+        # Get the remaining columns as a DataFrame
+        data = df.iloc[:, 1:]
+
+        if debug:
+
+            print("Loaded KDD99 dataset successfully. Returning data and target variables.")
+            print("Link to dataset: https://archive.ics.uci.edu/ml/datasets/KDD+Cup+1999+Data")
+            print(f"Data: {data.shape}")
+            print(f"Target: {target.shape}")
+
+        return data,target
+
+    # link to dataset http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz
+    url = 'http://kdd.ics.uci.edu/databases/kddcup99/kddcup.data.gz'
+    response = requests.get(url, stream=True)
+
+    if response.status_code == 200:
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024
+        t = tqdm(total=total_size, unit='iB', unit_scale=True, desc='Download KDD99 Dataset')
+
+        file_buffer = io.BytesIO()
+        for data in response.iter_content(block_size):
+            t.update(len(data))
+            file_buffer.write(data)
+        t.close()
+
+        file_buffer.seek(0)
+
+        with gzip.open(file_buffer) as the_file:
+            # Load the full CSV into a DataFrame
+            print("Load CSV into DataFrame")
+            the_file.seek(0)
+            kdd9 = pd.read_csv(the_file, header=None)
+
+            # Encode the all the categorical columns
+            from sklearn.preprocessing import LabelEncoder
+            le = LabelEncoder()
+            for col in kdd9.select_dtypes(include='object').columns:
+                kdd9[col] = le.fit_transform(kdd9[col])
+            
+
+            # Rename the last column to 'target' and separate it as a Series
+            target = kdd9.iloc[:, -1]
+            target.name = 'target'
+
+            # Get the remaining columns as a DataFrame
+            data = kdd9.iloc[:, :-1]
+
+
+
+            if save_path:
+                # Ensure the save path exists
+                os.makedirs(save_path, exist_ok=True)
+
+                if debug:
+                    print(f"Saving dataset to {save_path}")
+
+                df = pd.concat([target, data], axis=1)
+                df.to_csv(os.path.join(save_path, 'kdd99.csv'), index=False)
+        
+            if debug:
+                print("="*100)
+                print("Loaded KDD99 dataset successfully. Returning data and target variables.")
+                print("Link to dataset: https://archive.ics.uci.edu/ml/datasets/KDD+Cup+1999+Data")
+                print(f"Data: {data.shape}")
+                print(f"Target: {target.shape}")
+                print("="*100)
+            
+            return data, target
+
+    else:
+        ValueError(f"Failed to download dataset from {url}. Status code: {response.status_code}")
+        return None
+    
+  
+  
--- tests/test_LoadDataset.py
+++ tests/test_LoadDataset.py
@@ -2,6 +2,7 @@ import unittest
 import context as LoadDataset
 
 class TestLoadSusy(unittest.TestCase):
+
     def test_load_susy(self):
 
 
@@ -62,6 +63,19 @@ class TestLoadSusy(unittest.TestCase):
                 data, target = LoadDataset.load_iris(load_path='./misc/data')
                 self.assertIsNotNone(data)
                 self.assertIsNotNone(target)
+
+            
+    def test_load_kdd99(self):
+                    
+                    # Test saving the dataset to a provided path
+                    data, target = LoadDataset.load_kdd99(debug=True, save_path='./misc/data')
+                    self.assertIsNotNone(data)
+                    self.assertIsNotNone(target)
+            
+                    # Test loading the dataset from a provided path
+                    data, target = LoadDataset.load_kdd99(load_path='./misc/data')
+                    self.assertIsNotNone(data)
+                    self.assertIsNotNone(target)
 
 
 
